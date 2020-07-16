21d42afcb7b6def9dba903601b9ced43e6ee0ab5
chichengyu <31581269+chichengyu@users.noreply.github.com>
2020-07-16 15:21:46 +0800

    Update UploadController.java

--- qingcheng_parent/qingcheng_web_manager/src/main/java/cn/qingcheng/exception/file/UploadController.java
+++ qingcheng_parent/qingcheng_web_manager/src/main/java/cn/qingcheng/exception/file/UploadController.java
@@ -82,7 +82,7 @@ public class UploadController {
         // Endpoint以杭州为例，其它Region请按实际情况填写。
         String endpoint = "http://" + host;
         // 云账号AccessKey有所有API访问权限，建议遵循阿里云安全最佳实践，创建并使用RAM子账号进行API访问或日常运维，请登录 https://ram.console.aliyun.com 创建。
-        String accessKeyId = "LTAI4Fh7azVyfYdzkkLZwSVU";
+        String accessKeyId = "LTAI4Fh7azVyfYdzkkLZwSVUxx";
         String accessKeySecret = "m6iAuji31RjAUgKgDSWRxuCRUFTHIYxx";
         // 创建OSSClient实例。
         OSS ossClient = new OSSClientBuilder().build(endpoint, accessKeyId, accessKeySecret);
